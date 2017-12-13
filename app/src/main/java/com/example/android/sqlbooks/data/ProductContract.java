7eacb655d116a524d416652895261b501f746280
pkennedy89 <patrickken@gmail.com>
2017-12-13 20:41:55 +0000

    added contract and dbhelper

---
+++ app/src/main/java/com/example/android/sqlbooks/data/ProductContract.java
@@ -0,0 +1,25 @@
+package com.example.android.sqlbooks.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by pkennedy on 12/13/17.
+ */
+
+public final class ProductContract {
+
+    public static abstract class ProductEntry implements BaseColumns{
+
+        public static final String TABLE_NAME = "product";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_IMAGE = "image";
+        public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
+        public static final String COLUMN_SUPPLIER_EMAIL = "supplier_email";
+        public static final String COLUMN_SUPPLIER_PHONE = "supplier_price";
+
+    }
+}
---
+++ app/src/main/java/com/example/android/sqlbooks/data/ProductDbHelper.java
@@ -0,0 +1,47 @@
+package com.example.android.sqlbooks.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by pkennedy on 12/13/17.
+ */
+
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "product.db";
+
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + ProductContract.ProductEntry.TABLE_NAME + " (" +
+                    ProductContract.ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    ProductContract.ProductEntry.COLUMN_NAME + " TEXT NOT NULL," +
+                    ProductContract.ProductEntry.COLUMN_PRICE + " INTEGER NOT NULL," +
+                    ProductContract.ProductEntry.COLUMN_QUANTITY + " INTEGER NOT NULL," +
+                    ProductContract.ProductEntry.COLUMN_IMAGE + " BLOB," +
+                    ProductContract.ProductEntry.COLUMN_SUPPLIER_NAME + " TEXT NOT NULL," +
+                    ProductContract.ProductEntry.COLUMN_SUPPLIER_EMAIL + " TEXT NOT NULL," +
+                    ProductContract.ProductEntry.COLUMN_SUPPLIER_PHONE + " TEXT NOT NULL);";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + ProductContract.ProductEntry.TABLE_NAME;
+}
