0b422cc98c7b4b240dad5f19a75752547ce8b5cf
pkennedy89 <patrickken@gmail.com>
2017-12-21 19:27:23 +0000

    changed list_item

--- app/src/main/java/com/example/android/sqlbooks/EditorActivity.java
+++ app/src/main/java/com/example/android/sqlbooks/EditorActivity.java
@@ -1,8 +1,37 @@
 package com.example.android.sqlbooks;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 /**
  * Created by pkennedy on 12/21/17.
  */
 
-public class EditorActivity {
+public class EditorActivity extends AppCompatActivity {
+
+    private static final int EXISTING_PRODUCT_LOADER = 0;
+
+    /** Content URI for the existing pet (null if it's a new pet) */
+    private Uri PRODUCT_URI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        PRODUCT_URI = intent.getParcelableExtra("PRODUCT_URI");
+
+        Log.d("EditorActivity", "URI: " + PRODUCT_URI);
+
+        if (PRODUCT_URI == null){
+            getSupportActionBar().setTitle(R.string.add_product);
+        } else{
+            getSupportActionBar().setTitle(R.string.edit_product);
+        }
+}
+
 }
